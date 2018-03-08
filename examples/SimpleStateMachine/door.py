ea7f478f9754a7bca2addd4521ebba20bc18c1d2
haubschueh <marco.weber.abcxyz@gmail.com>
2018-03-08 17:24:42 +0100

    bugfix my door

--- examples/SimpleStateMachine/door.py
+++ examples/SimpleStateMachine/door.py
@@ -2,7 +2,7 @@ from door_states import Opened
 
 class door(object):
     def __init__(self):
-        self.state = LockedState()
+        self.state = Opened()
 
     def open(self):
         self.state = self.state.on_event('open')
