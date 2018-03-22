237ea7f41830548ce55915f7dc54f3fd2852a6a6
haubschueh <marco.weber.abcxyz@gmail.com>
2018-03-22 08:53:58 +0100

    added the answer commands to the language set + implementation

--- communication/FreedomInterface.py
+++ communication/FreedomInterface.py
@@ -30,5 +30,30 @@ class FreedomInterface:
             FreedomInterface.__instance = self
             FreedomInterface.__serialCommunicator = SerialCommunicator.getInstance()
 
+    def __ackLoop(self, command):
+        ack = ""
+        while ack != Commands.ACKNOWLEDGE:
+            self.__serialCommunicator.transmit(command)
+            ack = self.__serialCommunicator.receive()
+
     def hello(self):
-        self.__serialCommunicator.transmit(Commands.HELLO)
+        while True:
+            self.__serialCommunicator.transmit(Commands.HELLO)
+            ack = self.__serialCommunicator.receive()
+            if ack == Commands.ACKNOWLEDGE:
+                break
+
+    def drive(self):
+        self.__serialCommunicator.transmit(Commands.DRIVE)
+
+    def goto_cube(self):
+        self.__serialCommunicator.transmit(Commands.GO_DISTANCE)
+
+    def lift_cube(self):
+        self.__ackLoop(Commands.LIFT_LOAD)
+        fin = ""
+        while fin != Commands.FINISHED:
+            fin = self.__serialCommunicator.receive()
+
+    def put_cube(self):
+        self.__serialCommunicator.transmit(Commands.PUT_LOAD)
