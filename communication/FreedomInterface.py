2a93981342ae14ee54c7dcaaf7b47169c62dcf0b
haubschueh <marco.weber.abcxyz@gmail.com>
2018-04-06 09:09:29 +0200

    Reimplemented the methods which can be called according to the new language set.

--- communication/FreedomInterface.py
+++ communication/FreedomInterface.py
@@ -33,54 +33,81 @@ class FreedomInterface:
         ack = ""
         while ack != Commands.ACKNOWLEDGE:
             ack = self.__serialCommunicator.receive()
+        # while True:
+        #     ack = self.__serialCommunicator.receive()
+        #     if ack == Commands.ACKNOWLEDGE:
+        #         break
 
-    def __waitForAnswer(self):
-        while True:
-            ack = self.__serialCommunicator.receive()
-            if ack == Commands.ACKNOWLEDGE:
-                break
+    def __waitForFin(self):
+        fin = ""
+        while fin != Commands.FINISHED:
+            fin = self.__serialCommunicator.receive()
 
     """
     Initialize the communication and handles the start signal.
     """
     def hello(self):
         self.__serialCommunicator.transmit(Commands.HELLO)
-        self.__waitForAnswer()
+        self.__waitForAck()
         # wait for start signal
+        self.__waitForFin()
 
     """
     Commands to drive, stop and let the robot drive a fix distance.
     """
     def drive(self):
         self.__serialCommunicator.transmit(Commands.DRIVE)
-        self.__waitForAck()
+        #self.__waitForAck()
+
+    def driveSlow(self):
+        self.__serialCommunicator.transmit(Commands.DRIVE_SLOW)
+        #self.__waitForAck()
+
+    def driveFast(self):
+        self.__serialCommunicator.transmit(Commands.DRIVE_FAST)
+        #self.__waitForAck()
 
     def stop(self):
         self.__serialCommunicator.transmit(Commands.STOP)
-        self.__waitForAck()
+        #self.__waitForAck()
 
     def goto_cube(self):
         self.__serialCommunicator.transmit(Commands.GO_DISTANCE)
         self.__waitForAck()
-        # wait for finish signal
+        self.__waitForFin()
 
     """
     Commands to lift and lower the payload.
     """
     def lift_cube(self):
-        self.__ackLoop(Commands.LIFT_LOAD)
-        self.__waitForAnswer()
-        # wait for finish signal
+        self.__serialCommunicator.transmit(Commands.LIFT_LOAD)
+        self.__waitForAck()
+        self.__waitForFin()
 
     def put_cube(self):
         self.__serialCommunicator.transmit(Commands.PUT_LOAD)
-        self.__waitForAnswer()
-        # wait for finish signal
+        self.__waitForAck()
+        self.__waitForFin()
 
     """
     Gets the coordinates from the cube.
     """
-    def position(self):
-        self.__ackLoop(Commands.POSITION)
-        self.__waitForAnswer()
-        # get Position Data
+    def getCubePosition(self):
+        self.__serialCommunicator.transmit(Commands.POS_X)
+        x = self.__serialCommunicator.receive()
+        self.__serialCommunicator.transmit(Commands.POS_Z)
+        z = self.__serialCommunicator.receive()
+        return x
+
+    def wait(self):
+        ans = ""
+        while ans != Commands.FRDM_START:
+            ans = self.__serialCommunicator.receive()
+
+    def cube(self):
+        ans = ""
+        while ans != Commands.FRDM_CUBEUP:
+            ans = self.__serialCommunicator.receive()
+
+    def finish(self):
+        self.__serialCommunicator.transmit(Commands.FINISH)
