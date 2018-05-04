525fa927b592d9f5b08696380b84534da5c4028c
haubschueh <marco.weber.abcxyz@gmail.com>
2018-05-04 13:47:01 +0200

    Learned something about the uri config. Now the example works.

--- examples/Pyro/greeting-client.py
+++ examples/Pyro/greeting-client.py
@@ -1,7 +1,7 @@
 import Pyro4
 
-uri = input("192.168.10.8").strip()
-name = input("Marco").strip()
+uri = input("What is the Pyro uri of the greeting object? ").strip()
+name = input("What is your name? ").strip()
 
 greeting_maker = Pyro4.Proxy(uri)         # get a Pyro proxy to the greeting object
 print(greeting_maker.get_fortune(name))   # call method normally
--- examples/Pyro/greeting-server.py
+++ examples/Pyro/greeting-server.py
@@ -6,8 +6,8 @@ class GreetingMaker(object):
         return "Hello, {0}. Here is your fortune message:\n" \
                "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+daemon = Pyro4.Daemon(host="192.168.10.8", port=34383)  # make a Pyro daemon
+uri = daemon.register(GreetingMaker)                    # register the greeting maker as a Pyro object
 
-print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+print("Ready. Object uri =", uri)                       # print the uri so we can use it in the client later
+daemon.requestLoop()                                    # start the event loop of the server to wait for calls
