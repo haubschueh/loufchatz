5f51eb5f8d8944610968b041f1e6346d139dd32f
haubschueh <marco.weber.abcxyz@gmail.com>
2018-05-06 23:04:39 +0200

    Moved the pyro stuff to a separate thread. Added a facade that pyro can not block the interface to the freedomboard.

---
+++ client/ClientInterface.py
@@ -0,0 +1,24 @@
+from client.PyroFreedomInterfaceFacade import PyroFacade
+import threading
+
+"""
+
+"""
+class ClientInterface(threading.Thread):
+
+    __daemon = None
+    __IP = "192.168.10.8"
+    __PORT = 34383
+    __pyroUri = ""
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        pyroFacade = PyroFacade()
+        self.daemon = Pyro4.Daemon(host=__IP, port=__PORT)
+        self.pyroUri = self.daemon.register(pyroFacade)
+
+    def run(self):
+        self.daemon.requestLoop()
+
+    def getUri(self):
+        return self.pyroUri
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from log.LoggerFactory import LoggerFactory
 from communication.FreedomInterface import FreedomInterface
 from targetrecognition.targetrec import TargetRec
-from position.position import PositionUpdater
+#from position.position import PositionUpdater
+from client.ClientInterface import ClientInterface
 import time
 
 """
@@ -10,18 +11,21 @@ import time
 log = LoggerFactory.getLogger('main')
 freedomBoard = FreedomInterface.getInstance()
 targetRec = TargetRec()
-positionUpdater = PositionUpdater(LoggerFactory.getLogger('Position'), freedomBoard)
+#positionUpdater = PositionUpdater(LoggerFactory.getLogger('Position'), freedomBoard)
+clientInterface = ClientInterface()
+uri = clientInterface.getUri()
 
 log.info('Run started.')
+log.info('Client uri is: %s', uri)
 freedomBoard.waitForStart()
 freedomBoard.waitForCube()
 #Continues to run when the target is found
-positionUpdater.start()
+#positionUpdater.start()
 freedomBoard.drive()
 targetRec.searchSquare()
 time.sleep(2.7)
 freedomBoard.stop()
-positionUpdater.stop()
+#positionUpdater.stop()
 time.sleep(.300)
 freedomBoard.finish()
 log.info('Run finished.')
