57cc4e70fa8f4d9698ae9716b622a8a283a30d65
haubschueh <marco.weber.abcxyz@gmail.com>
2018-05-09 09:44:19 +0200

    Replaced the first implementation of the client with the pygubu gui

--- client/client.py
+++ client/client.py
@@ -1,25 +1,68 @@
 import tkinter as tk
+import pygubu
+import os
+import Pyro4
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+class Application:
+    def __init__(self, master):
+        pyroUri = input("Enter Pyro uri: ").strip()
+        self.loufchatz = Pyro4.Proxy(pyroUri)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('client.ui')
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
+        img_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.', 'images')
+        img_path = os.path.abspath(img_path)
+        builder.add_resource_path(img_path)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+        self.mainwindow = builder.get_object('window', master)
+        builder.connect_callbacks(self)
 
-root = tk.Tk()
-root.tk.call('tk', 'scaling', 2.0)
-app = Application(master=root)
-app.mainloop()
+    def on_BackwardFast_clicked(self):
+        self.loufchatz.driveBackwardFast()
+
+    def on_Backward_clicked(self):
+        self.loufchatz.driveBackward()
+
+    def on_BackwardSlow_clicked(self):
+        self.loufchatz.driveBackwardSlow()
+
+    def on_Stop_clicked(self):
+        self.loufchatz.stop()
+
+    def on_ForwardSlow_clicked(self):
+        self.loufchatz.driveSlow()
+
+    def on_Forward_clicked(self):
+        self.loufchatz.drive()
+
+    def on_ForwardFast_clicked(self):
+        self.loufchatz.driveFast()
+
+    def on_HopperDown_clicked(self):
+        self.loufchatz.hopperDown()
+
+    def on_HopperStop_clicked(self):
+        self.loufchatz.hopperStop()
+
+    def on_HopperUp_clicked(self):
+        self.loufchatz.hopperUp()
+
+    def on_AttachCube_clicked(self):
+        self.loufchatz.attachCube()
+
+    def on_ReleaseCube_clicked(self):
+        self.loufchatz.releaseCube()
+
+    def on_State_clicked(self):
+        self.loufchatz.getState()
+
+    def on_Reset_clicked(self):
+        self.loufchatz.reset()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.tk.call('tk', 'scaling', 2.0)
+    app = Application(root)
+    root.mainloop()
