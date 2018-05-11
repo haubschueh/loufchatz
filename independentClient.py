20ec80cff708ae96c6ce69c63a696bb5a181622c
haubschueh <marco.weber.abcxyz@gmail.com>
2018-05-11 08:51:30 +0200

    Script runs out, added an endless while true with break on keypress

--- independentClient.py
+++ independentClient.py
@@ -1,5 +1,11 @@
 from client.ClientInterface import ClientInterface
+import time
+
 clientInterface = ClientInterface()
 uri = clientInterface.getUri()
 clientInterface.start()
 print('Client uri is: %s', uri)
+while True:
+    time.sleep(1)
+    if cv2.waitKey(1) == 13:
+        break
