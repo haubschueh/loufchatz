8415a950e1bf077b02723e9cba075530182f18f2
haubschueh <marco.weber.abcxyz@gmail.com>
2018-05-17 17:18:19 +0200

    Updated the client to update the position display. Textinputs had to be reenabled, method to modify the value of the text objects added and a thread class to call the method had to be implemented

---
+++ client/PositionUpdater.py
@@ -0,0 +1,15 @@
+import threading
+import time
+
+class PositionUpdater(threading.Thread):
+    def __init__(self, remoteClass, userInterface):
+        threading.Thread.__init__(self)
+        self.loufchatz = remoteClass
+        self.gui = userInterface
+
+    def run(self):
+        while True:
+            x = self.loufchatz.getCubePositionX()
+            z = self.loufchatz.getCubePositionZ()
+            self.gui.updatePosition(x, z)
+            time.sleep(.500)
--- client/client.py
+++ client/client.py
@@ -2,21 +2,36 @@ import tkinter as tk
 import pygubu
 import os
 import Pyro4
+from PositionUpdater import PositionUpdater
 
 class Application:
     def __init__(self, master):
+        self.master = master
+
         pyroUri = input("Enter Pyro uri: ").strip()
         self.loufchatz = Pyro4.Proxy(pyroUri)
 
-        self.builder = builder = pygubu.Builder()
-        builder.add_from_file('client.ui')
+        self.builder = pygubu.Builder()
+        self.builder.add_from_file('client.ui')
 
         img_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.', 'images')
         img_path = os.path.abspath(img_path)
-        builder.add_resource_path(img_path)
+        self.builder.add_resource_path(img_path)
 
-        self.mainwindow = builder.get_object('window', master)
-        builder.connect_callbacks(self)
+        self.mainwindow = self.builder.get_object('window', master)
+        self.builder.connect_callbacks(self)
+
+        pos = PositionUpdater(self.loufchatz, self)
+        pos.start()
+
+    def updatePosition(self, X, Z):
+        TextFieldX = self.builder.get_object('PosX')
+        TextFieldZ = self.builder.get_object('PosZ')
+        TextFieldX.delete('1.0', tk.END)
+        TextFieldX.insert(tk.INSERT, X)
+        TextFieldZ.delete('1.0', tk.END)
+        TextFieldZ.insert(tk.INSERT, Z)
+        self.master.update()
 
     def on_BackwardFast_clicked(self):
         self.loufchatz.driveBackwardFast()
